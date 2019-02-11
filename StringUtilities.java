53f857947fd838de665ed1a4c659e6f62464fcca
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-11 11:06:41 -0500

    Shuchi committing StringUtilities

--- StringUtilities.java
+++ StringUtilities.java
@@ -1,11 +1,11 @@
-
+import java.lang.StringBuilder;
 public class StringUtilities {
     /**
      * @param input value to be returned
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return baseValue+valueToBeAdded;
     }
 
     /**
@@ -22,7 +22,14 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String reverse = "";
+        
+        for (int i=valueToBeReversed.length(); i>0;i--){
+        
+        reverse = reverse+valueToBeReversed.charAt(i-1);
+    }
+        
+        return reverse;
     }
 
     /**
@@ -30,7 +37,9 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        
+        
+        return word.charAt(word.length()/2);
     }
 
     /**
@@ -39,14 +48,40 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-        return null;
-    }
+        
+        char [] charr = value.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i=0 ; i<charr.length; i++){
+            if (charr[i]!=charToRemove){
+                sb.append(charr[i]);
+                
+                
+                
+            }
+        }
+            return sb.toString();
+            
+        }
+        
+    
+    
 
     /**
      * @param sentence String delimited by spaces representative of a sentence
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
-    }
+        
+        String [] strarr = sentence.split(" ");
+        
+            
+            String last = strarr[strarr.length-1];
+            
+            
+        
+        
+    
+     
+        return last;
+}
 }
